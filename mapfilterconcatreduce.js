6d1977077eabdbbe9236efd7a8c7b91661e785a5
Vilhelm Falkenmark <vilhelmfalkenmark@Vilhelms-MacBook-Pro.local>
2017-08-31 11:46:29 +0200

    first commit

---
+++ mapfilterconcatreduce.js
@@ -0,0 +1,115 @@
+const person = {
+  name: 'Name',
+  age: 100,
+  male: false,
+  climbs: false
+}
+
+const people = [
+  {
+    name: 'Fredrik',
+    age: 25,
+    male: true,
+    climbs: false
+  }, {
+    name: 'Ville',
+    age: 31,
+    male: true,
+    climbs: true
+  }, {
+    name: 'Britta',
+    age: 34,
+    male: false,
+    climbs: false
+  }, {
+    name: 'Johan',
+    age: 38,
+    male: true,
+    climbs: true
+  }, {
+    name: 'Thierry',
+    age: 30,
+    male: true,
+    climbs: true
+  }
+];
+
+
+
+
+// //////////////////////////////////////////////
+/*
+Task 1.
+
+Create an array that only contains people older
+than 30
+*/
+// /////////////////////////////////////////////
+
+
+
+// //////////////////////////////////////////////
+/*
+Task 2.
+
+Create an array where we take everything from
+The original array. Dont change anything
+*/
+// /////////////////////////////////////////////
+
+
+
+// //////////////////////////////////////////////
+/*
+Task 3.
+
+Create an array where we add Carl
+with the help of the person Object
+*/
+// /////////////////////////////////////////////
+
+
+// //////////////////////////////////////////////
+/*
+Task 4.
+
+Create an array where we change the name of the
+person to Mike if that person climbs
+otherwise name it Hans
+*/
+// /////////////////////////////////////////////
+
+
+
+// //////////////////////////////////////////////
+/*
+Task 5.
+
+Create an array where we remove everyone that doesnt climb.
+After that add florian to the new array
+*/
+// /////////////////////////////////////////////
+
+
+// //////////////////////////////////////////////
+/*
+Task 6.
+
+Create a new variable where we calculate the age
+of everyone younger than 35 (< 35 )
+*/
+// /////////////////////////////////////////////
+
+
+// //////////////////////////////////////////////
+/*
+Task 7.
+
+Create an array where we
+
+1. Add Simon
+2. Remove every climber
+3. Create an array only containing the ages (As Ints, not objects)
+4. Returns the total age of everyone remaining
+*/
+// /////////////////////////////////////////////
